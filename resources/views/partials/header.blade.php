0127ec641c01799f63866cc236807b79b374a73c
vittosba <vitsbardella@gmail.com>
2022-01-19 18:55:53 +0100

    header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,10 @@
-<header>
-    HEADER
+<header class="container">
+    <div class="main-header">
+        <img src="{{ asset('images/dc-logo.png') }}" alt="">
+
+        <ul>
+            <li class="active">Comics</li>
+            <li>News</li>
+        </ul>
+    </div>
 </header>
