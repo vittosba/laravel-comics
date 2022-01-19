cd0a2588e7acda25da01df27294db0a99d09a3e9
vittosba <vitsbardella@gmail.com>
2022-01-19 18:12:41 +0100

    homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,16 @@
 @extends('layouts.main')
 
 @section('main-content')
-    <div class="cards">
-        
-    </div>
+    @if (! empty($comics))
+        <ul class="cards">
+            @foreach ($comics as $comic)
+                <li class="card">
+                    <img src="{{ $comic['thumb'] }}" alt="">
+                    <div class="comic-title">{{ $comic['series'] }}</div>
+                </li>
+            @endforeach
+        </ul>        
+    @else
+        <h2>Nessun comics trovato</h2>
+    @endif
 @endsection
