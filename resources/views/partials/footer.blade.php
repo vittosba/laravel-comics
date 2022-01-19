649998a76728860fc31ff58538a8492d70a4a705
vittosba <vitsbardella@gmail.com>
2022-01-19 19:11:19 +0100

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,18 @@
 <footer>
-    FOOTER
+    <div class="top-footer"></div>
+    <div class="bottom-footer">
+        <div class="container">
+            <div class="left-footer">
+                <button>Sign-up now!</button>
+            </div>
+            <div class="right-footer">
+                <div class="follow">Follow us</div>
+                <img src="{{ asset('images/footer-facebook.png') }}" alt="">
+                <img src="{{ asset('images/footer-twitter.png') }}" alt="">
+                <img src="{{ asset('images/footer-youtube.png') }}" alt="">
+                <img src="{{ asset('images/footer-pinterest.png') }}" alt="">
+                <img src="{{ asset('images/footer-periscope.png') }}" alt="">
+            </div>
+        </div>
+    </div>
 </footer>
